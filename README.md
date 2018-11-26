d01b4b07ec820ef7812044f8af6cba78c29cf279
Rogier Spieker <rogier.spieker@magneds.com>
2018-11-26 09:42:02 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,58 @@
+# Hapi PDF Plugin
+
+Create a PDF file from any web page
+
+## Installation
+
+```
+$ npm install --save @magneds/hapi-plugin-pdf
+```
+
+## Usage
+
+As the name might have suggested already, this package is a plugin for the Hapi framework, simply registering the package as plugin will suffice.
+
+### Using `@magneds/hapi-server`
+
+```js
+const HapiServer = require('@magneds/node-hapi-server');
+const HapiPluginPDF = require('@magneds/hapi-plugin-pdf');
+
+new HapiServer()
+	.configure({ host: 'localhost', port: 3000 })
+	.plugin(HapiPluginPDF)
+	.start()
+	.then(() => console.log(`Running @${server.info.uri}`));
+```
+
+### Using `hapi`
+
+```js
+const Hapi = require('hapi');
+const HapiPluginPDF = require('@magneds/hapi-plugin-pdf');
+
+const server = Hapi.server({ host: 'localhost', port: 3000 });
+
+const init = async () => {
+	await server.register(HapiPluginPDF);
+	await server.start();
+
+	console.log(`Running @${server.info.uri}`);
+};
+
+init();
+```
+
+## API
+
+The pdf plugins registers a single route and works with both the `GET` and `POST` request methods.
+
+### `/pdf/render`
+
+The request is delegated to the provided url using the method used to call the `/pdf/render` endpoint, any GET and/or POST paramater as well as all headers are added to the delegated request.
+
+```
+GET http://localhost:3000/pdf/render?url=https://www.magneds.com
+```
+
+(Obviously it will look horrible, as it does not have a decent print stylesheet)
---
+++ source/index.js
@@ -0,0 +1,61 @@
+const Puppeteer = require('puppeteer');
+const Plugin = require('@magneds/hapi-plugin');
+const plugin = new Plugin(require('../package.json'));
+
+function serialize(obj) {
+	return Object.keys(obj)
+		.map(
+			(key) =>
+				`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`
+		)
+		.join('&');
+}
+
+plugin.name = 'pdf';
+plugin.prefix = '/pdf';
+plugin.register = (server) =>
+	server.route([
+		{
+			method: ['GET', 'POST'],
+			path: '/render',
+			async handler(request, h) {
+				const { method, query, payload, headers } = request;
+				const browser = await Puppeteer.launch();
+				const page = await browser.newPage();
+				const url = (payload || {}).url || query.url;
+				const param = serialize(query);
+
+				if (method === 'post') {
+					await page.setRequestInterception(true);
+
+					page.on('request', (request) => {
+						const override = {
+							method: 'POST',
+							postData: serialize(payload),
+							headers: {
+								...headers,
+								'accept-encoding': undefined,
+								'content-length': undefined,
+							},
+						};
+
+						return request.continue(override);
+					});
+				}
+
+				await page.goto(url + (param ? `?${param}` : ''), {
+					waitUntil: 'load',
+				});
+				const pdf = await page.pdf({
+					format: 'A4',
+					printBackground: true,
+				});
+
+				return h
+					.response(pdf)
+					.header('content-type', 'application/pdf');
+			},
+		},
+	]);
+
+module.exports = plugin.export;
