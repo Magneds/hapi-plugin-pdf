0f751b32a6e77dfc55fd836ae45f06475d1cf548
Rogier Spieker <rogier.spieker@magneds.com>
2018-12-01 09:08:45 +0100

    Added tests

---
+++ test/source/hapi-server.js
@@ -0,0 +1,90 @@
+const HapiServer = require('@magneds/hapi-server');
+const Lab = require('lab');
+const { expect } = require('code');
+const { experiment, beforeEach, test } = (exports.lab = Lab.script());
+
+const HapiPluginPDF = require('../..');
+
+async function start(...plugins) {
+	return await new HapiServer().plugin(plugins).start();
+}
+
+experiment('HapiServer', () => {
+	experiment('default registration', () => {
+		test('GET request', { timeout: 10000 }, async () => {
+			const server = await start(HapiPluginPDF);
+			const url = encodeURIComponent(
+				'https://postman-echo.com/get?foo=bar&baz=qux'
+			);
+			const get = await server.inject({
+				method: 'GET',
+				url: `/pdf/render?url=${url}`
+			});
+
+			expect(get).to.be.object();
+			expect(get.statusCode).to.equal(200);
+			expect(get.headers).to.contain('content-type');
+			expect(get.headers['content-type']).to.equal('application/pdf');
+		});
+
+		test('POST request', { timeout: 10000 }, async () => {
+			const server = await start(HapiPluginPDF);
+			const post = await server.inject({
+				method: 'POST',
+				url: '/pdf/render',
+				payload: {
+					url: 'https://postman-echo.com/post',
+					foo: 'bar',
+					baz: 'qux'
+				}
+			});
+
+			expect(post).to.be.object();
+			expect(post.statusCode).to.equal(200);
+			expect(post.headers).to.contain('content-type');
+			expect(post.headers['content-type']).to.equal('application/pdf');
+		});
+	});
+
+	experiment('path prefix override', () => {
+		test('GET request', { timeout: 10000 }, async () => {
+			const server = await start({
+				...HapiPluginPDF,
+				routes: { prefix: '/override/path' }
+			});
+			const url = encodeURIComponent(
+				'https://postman-echo.com/get?foo=bar&baz=qux'
+			);
+			const get = await server.inject({
+				method: 'GET',
+				url: `/override/path/render?url=${url}`
+			});
+
+			expect(get).to.be.object();
+			expect(get.statusCode).to.equal(200);
+			expect(get.headers).to.contain('content-type');
+			expect(get.headers['content-type']).to.equal('application/pdf');
+		});
+
+		test('POST request', { timeout: 10000 }, async () => {
+			const server = await start({
+				...HapiPluginPDF,
+				routes: { prefix: '/override/path' }
+			});
+			const post = await server.inject({
+				method: 'POST',
+				url: '/override/path/render',
+				payload: {
+					url: 'https://postman-echo.com/post',
+					foo: 'bar',
+					baz: 'qux'
+				}
+			});
+
+			expect(post).to.be.object();
+			expect(post.statusCode).to.equal(200);
+			expect(post.headers).to.contain('content-type');
+			expect(post.headers['content-type']).to.equal('application/pdf');
+		});
+	});
+});
